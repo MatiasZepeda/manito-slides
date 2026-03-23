d349923e7d3fceb0eb978cdc3a9d6a904733da73
Matias Zepeda <matiaszepeda45@gmail.com>
2026-03-22 20:20:06 -0500

    Initial Manito pre-lanzamiento slides

---
+++ components/SlideHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="slide-header">
+    <span class="slide-header-title">{{ title }}</span>
+    <span class="slide-header-counter">{{ counter }}</span>
+  </div>
+  <div class="slide-content">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  counter: String,
+})
+</script>
